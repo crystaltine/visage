83ebce691d5841bf4501743bf2e082df8d88c011
akshaisrin <akshaisrin@gmail.com>
2024-04-04 11:52:36 -0700

    add resizing func to ui render

--- example2_script.py
+++ example2_script.py
@@ -6,3 +6,18 @@ read_vis("example2.vis")
 
 document = Globals.__vis_document__
 document.mount()
+
+last_w = document.term.width
+last_h = document.term.height
+
+while 1:
+    curr_w = document.term.width
+    curr_h = document.term.height
+
+    if (curr_w != last_w) or (curr_h != last_h):
+        # dims changed, rerender and remomorize
+        last_w = document.term.width
+        last_h = document.term.height
+        document.client_right = document.term.width
+        document.client_bottom = document.term.height
+        document.render()
